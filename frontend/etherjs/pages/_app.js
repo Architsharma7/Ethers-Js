b4a5f4b16bbc24fb581582562a0314a46b46f368
Architsharma7 <architsharma7911@gmail.com>
2023-03-10 20:19:49 +0530

    hooks added

--- frontend/etherjs/pages/_app.js
+++ frontend/etherjs/pages/_app.js
@@ -1,17 +1,32 @@
 import React from "react";
-import { configureChains, createClient, WagmiConfig, goerli, createStorage } from "wagmi";
+import { configureChains, createClient, WagmiConfig, goerli } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
+// import { publicProvider } from 'wagmi/providers/public'
 
 const { chains, provider } = configureChains(
   [goerli],
   [alchemyProvider({ apiKey: "kWaRU0Ob51cgzo0jkLJK3QAeilm4Pu14" })]
 );
 
+//optionally you can do this (pt.1) 
+// const { chains, provider } = configureChains(
+//   [mainnet, optimism],
+//   [
+      //  alchemyProvider({ apiKey: 'yourAlchemyApiKey' }),
+      //  publicProvider(),
+      // for multiple providers
+      //  infuraProvider({ apiKey: 'yourInfuraApiKey' })
+      // ],
+// )
+
 const client = createClient({
   autoConnect: true,
   provider,
   //The default strategy to persist and cache data.
   //storage: createStorage({ storage: window.localStorage }),
+
+  //if done the pt.1 do this,
+  // connectors: [new InjectedConnector({ chains })],
 });
 
 function MyApp({ Component, pageProps }) {
--- frontend/etherjs/pages/wagmi.js
+++ frontend/etherjs/pages/wagmi.js
@@ -1,26 +1,44 @@
-import React from 'react'
-import { useAccount, useConnect} from 'wagmi'
-import { InjectedConnector } from 'wagmi/connectors/injected'
+import React from "react";
+import { useAccount, useBlockNumber, useConnect, useContract } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { goerli } from "wagmi";
+import { useBalance } from "wagmi";
 
 const Wagmi = () => {
+  useAccount;
+  const { address, isConnected } = useAccount();
+  const connector = new InjectedConnector({
+    chains: [goerli],
+  });
+  // useConnect
+  const {connect} = useConnect({
+    connector,
+  });
+  useBalance;
+  const { data, isError, isLoading } = useBalance({
+    address: `0x9B855D0Edb3111891a6A0059273904232c74815D`,
+    chainId: 5,
+  });
+  // useBlockNumber
+  const { block } = useBlockNumber();
 
-    const { address, isConnected } = useAccount()
-    const { connect } = useConnect({
-        connector: new InjectedConnector(),
-      })
+  // useContract
+  const contract = useContract({
+    address: `0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e`,
+    abi: ``,
+  })
 
   return (
     <div>
-        {
-            isConnected ? (
-                <p>wallet connected : {address}</p>
-            ) : 
-            (
-                <button onClick={connect}>Connect Wallet</button>
-            )
-        }
+      {isConnected ? (
+        <p>
+          wallet connected : {address} {data?.formatted} {block} 
+        </p>
+      ) : (
+        <button onClick={connect}>Connect Wallet</button>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Wagmi;
